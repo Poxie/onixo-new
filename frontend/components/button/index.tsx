cd2a979e79f9ffae27efbcf34f106db88ada2ee6
= <poxen12@hotmail.com>
2023-04-23 16:01:21 +0200

    Added styling to the navbar, added login button

---
+++ frontend/components/button/index.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+import React, { AnchorHTMLAttributes, ReactElement } from 'react';
+import styles from './Button.module.scss';
+
+type ButtonProps = {
+    children?: any;
+    buttonType?: 'button' | 'submit';
+    type?: 'primary' | 'secondary' | 'hollow' | 'transparent';
+    className?: string;
+    style?: React.CSSProperties;
+    onClick?: () => void;
+    href?: string;
+    ariaLabel?: string;
+    target?: AnchorHTMLAttributes<''>['target'];
+    disabled?: boolean;
+    external?: boolean;
+    icon?: ReactElement;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ children, style, onClick, href, ariaLabel, target, icon, buttonType='button', external=false, disabled=false, className='', type='primary' }, ref) => {
+    className = [
+        className,
+        styles['container'],
+        styles[type],
+        disabled ? styles['disabled'] : ''
+    ].join(' ');
+
+    const props = {
+        className,
+        style,
+        onClick,
+        disabled,
+        'aria-label': ariaLabel,
+        ref: ref as any
+    }
+
+    return href ? (
+        external ? (
+            <a 
+                href={href}
+                rel={'noreferrer'}
+                target={target}
+                {...props}
+            >
+                {icon}
+                {children}
+            </a>
+        ) : (
+            <Link target={target} href={href} {...props}>
+                {icon}
+                {children}
+            </Link>
+        )
+    ) : (
+        <button 
+            type={buttonType}
+            {...props}
+        >
+            {icon}
+            {children}
+        </button>
+    )
+});
+Button.displayName = 'Button';
+
+export default Button;
--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -1,22 +1,31 @@
 import Link from "next/link"
+import styles from './Navbar.module.scss';
+import Button from "../button";
 
 const TABS = ['Features', 'Premium', 'Invite'];
 
 export const Navbar = () => {
     return(
-        <>
-        <Link href="/">
-            {process.env.NEXT_PUBLIC_WEBSITE_NAME}
-        </Link>
-        <ul>
-            {TABS.map(tab => (
-                <li key={tab}>
-                    <Link href={`/${tab.toLowerCase()}`}>
-                        {tab}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-        </>
+        <div className={styles['container']}>
+            <div className={styles['left']}>
+                <Link className={styles['header']} href="/">
+                    {process.env.NEXT_PUBLIC_WEBSITE_NAME}
+                </Link>
+                <ul className={styles['tabs']}>
+                    {TABS.map(tab => (
+                        <li key={tab}>
+                            <Link className={styles['tab']} href={`/${tab.toLowerCase()}`}>
+                                {tab}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className={styles['buttons']}>
+                <Button>
+                    Login with Discord
+                </Button>
+            </div>
+        </div>
     )
 }
