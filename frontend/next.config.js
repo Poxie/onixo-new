066909526c175890c7926edc33b13ae89c0bfe80
= <poxen12@hotmail.com>
2023-04-29 16:55:24 +0200

    Redirecting /invite to discord invite page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/invite',
+        destination: 'https://discord.com/oauth2/authorize?client_id=814312727115071499&scope=bot&permissions=8',
+        permanent: true,
+        basePath: false
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
