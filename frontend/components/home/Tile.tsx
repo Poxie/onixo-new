a76eaa9ac4ab0ec809d708d39e9885e036ea5aad
= <poxen12@hotmail.com>
2023-04-30 02:01:19 +0200

    Animating in tiles when user scrolls to them

--- frontend/components/home/Tile.tsx
+++ frontend/components/home/Tile.tsx
@@ -1,9 +1,14 @@
+import { useRef } from 'react';
 import styles from './Home.module.scss';
 import { TileType } from './Tiles';
+import { useWithinView } from '@/hooks/useWithinView';
 
 export const Tile: React.FC<TileType & {
     reversed: boolean;
 }> = ({ title, text, icon, boldIndices, reversed }) => {
+    const ref = useRef<HTMLLIElement>(null);
+    const visible = useWithinView(ref);
+
     const newTitle = title.split(' ').map((part, index) => {
         if(boldIndices.includes(index)) return <span className="highlight">{part}</span>;
         return part + ' ';
@@ -11,10 +16,11 @@ export const Tile: React.FC<TileType & {
 
     const className = [
         styles['tile'],
-        reversed ? styles['reversed'] : ''
+        reversed ? styles['reversed'] : '',
+        !visible ? styles['hidden'] : ''
     ].join(' ');
     return(
-        <li className={className}>
+        <li className={className} ref={ref}>
             <div className={styles['tile-text']}>
                 <h2>
                     {newTitle}
