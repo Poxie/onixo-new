9f3acb8bec3f8d2704448a22fa66f0259d6c66ee
= <poxen12@hotmail.com>
2023-05-09 01:09:58 +0200

    Made navbar responsive on smaller devices

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -1,19 +1,35 @@
 import Link from "next/link"
 import styles from './Navbar.module.scss';
 import Button from "../button";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { HamIcon } from "@/assets/icons/HamIcon";
 
 const TABS = ['Features', 'Premium', 'Invite'];
 
 export const Navbar = () => {
+    const asPath = useRouter().asPath;
+    const [open, setOpen] = useState(false);
+
+    // If path change, make sure to close mobile nav
+    useEffect(() => setOpen(false), [asPath]);
+
+    const className = [
+        styles['container'],
+        open ? styles['open'] : ''
+    ].join(' ');
     return(
-        <div className={styles['container']}>
+        <div className={className}>
             <div className={styles['left']}>
                 <Link className={styles['header']} href="/">
                     {process.env.NEXT_PUBLIC_WEBSITE_NAME}
                 </Link>
                 <ul className={styles['tabs']}>
                     {TABS.map(tab => (
-                        <li key={tab}>
+                        <li 
+                            onClick={() => setOpen(false)}
+                            key={tab}
+                        >
                             <Link className={styles['tab']} href={`/${tab.toLowerCase()}`}>
                                 {tab}
                             </Link>
@@ -25,6 +41,12 @@ export const Navbar = () => {
                 <Button>
                     Login with Discord
                 </Button>
+                <div 
+                    className={styles['ham']}
+                    onClick={() => setOpen(!open)}
+                >
+                    <HamIcon />
+                </div>
             </div>
         </div>
     )
