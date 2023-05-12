40e6d8d952420a0d2e6582696d204824d304d075
= <poxen12@hotmail.com>
2023-05-12 13:11:15 +0200

    Added routes to fetch access token

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
+import CORS from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 
 import auth from './routes/auth';
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(CORS({ origin: 'http://localhost:3000' }));
 dotenv.config();
 
 app.use(auth);
