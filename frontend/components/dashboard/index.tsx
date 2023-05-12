a9021befbb4e2e2c17e3e7b37ba3eccfbca08167
= <poxen12@hotmail.com>
2023-05-12 19:34:30 +0200

    Added simple server selection

--- frontend/components/dashboard/index.tsx
+++ frontend/components/dashboard/index.tsx
@@ -1,7 +1,46 @@
+import Image from 'next/image';
+import styles from './Dashboard.module.scss';
+import { useAuth } from "@/contexts/auth"
+import { getGuildIcon } from '@/utils/getImages';
+import Button from '../button';
+
 export const Dashboard = () => {
+    const { guilds } = useAuth();
+    if(!guilds) return;
+
     return(
-        <div>
-            dash
-        </div>
+        <main className={styles['container']}>
+            <h1>
+                Select your server
+            </h1>
+            <ul className={styles['guild-list']}>
+                {guilds.map(guild => (
+                    <li className={styles['guild-item']}>
+                        <div className={styles['guild-item-banner']}>
+                            <Image 
+                                src={getGuildIcon(guild.id, guild.icon)}
+                                alt=""
+                                fill
+                            />
+                        </div>
+                        <div className={styles['guild-item-text']}>
+                            <Image 
+                                width={60}
+                                height={60}
+                                src={getGuildIcon(guild.id, guild.icon)}
+                                className={styles['guild-item-icon']}
+                                alt=""
+                            />
+                            <span>
+                                {guild.name}
+                            </span>
+                            <Button>
+                                Go to Dashboard
+                            </Button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </main>
     )
 }
--- frontend/utils/getImages.ts
+++ frontend/utils/getImages.ts
@@ -1,3 +1,6 @@
 export const getUserAvatar = (userId: string, avatar: string) => (
     `https://cdn.discordapp.com/avatars/${userId}/${avatar}.png`
 )
+export const getGuildIcon = (guildId: string, icon: string) => (
+    `https://cdn.discordapp.com/icons/${guildId}/${icon}.png`
+)
