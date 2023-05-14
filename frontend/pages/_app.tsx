247001703d14616e09075a9d19483e2491369653
= <poxen12@hotmail.com>
2023-05-15 00:09:24 +0200

    Added layout compatibility

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,13 +3,26 @@ import { Navbar } from '@/components/navbar'
 import { AuthProvider } from '@/contexts/auth'
 import { store } from '@/redux/store'
 import '@/styles/globals.css'
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import { ReactElement, ReactNode } from 'react'
 import { Provider } from 'react-redux'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
   return(
     <div className={inter['className']}>
       <Provider store={store}>
