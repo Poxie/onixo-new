0c73d2672ce482dfa1297e2e3ed31b6800c227a9
= <poxen12@hotmail.com>
2023-05-15 12:17:14 +0200

    Fetching guild channels

--- frontend/layouts/dashboard/index.tsx
+++ frontend/layouts/dashboard/index.tsx
@@ -1,11 +1,33 @@
 import styles from './DashboardLayout.module.scss';
-import { ReactElement } from "react"
+import { ReactElement, useEffect } from "react"
 import { Sidebar } from './Sidebar';
 import { Navbar } from './Navbar';
+import { useGuildId } from '@/hooks/useGuildId';
+import { useAuth } from '@/contexts/auth';
+import { Channel } from '@/types';
+import { setGuildChannels } from '@/redux/dashboard/actions';
+import { useAppDispatch, useAppSelector } from '@/redux/store';
+import { selectChannelsFetched } from '@/redux/dashboard/selectors';
 
 export const DashboardLayout: React.FC<{
     children: ReactElement | ReactElement[];
 }> = ({ children }) => {
+    const guildId = useGuildId();
+    const { get, token } = useAuth();
+    
+    const dispatch = useAppDispatch();
+    const fetched = useAppSelector(state => selectChannelsFetched(state, guildId))
+
+    // Fetching guild roles/channels on mount
+    useEffect(() => {
+        if(!token || !guildId || fetched) return;
+
+        get<Channel[]>(`/guilds/${guildId}/channels`, 'backend')
+            .then(channels => {
+                dispatch(setGuildChannels(guildId, channels));
+            })
+    }, [get, guildId, fetched]);
+    
     return(
         <div className={styles['dashboard']}>
             <Navbar />
