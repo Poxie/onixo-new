0f0ff62bffcd00a0dce4d8b56b1cb92f338e1566
= <poxen12@hotmail.com>
2023-05-15 04:38:37 +0200

    Removed dropdown skeleton interactivity

--- frontend/layouts/dashboard/SidebarDropdownSkeleton.tsx
+++ frontend/layouts/dashboard/SidebarDropdownSkeleton.tsx
@@ -3,7 +3,7 @@ import styles from './DashboardLayout.module.scss';
 
 export const SidebarDropdownSkeleton = () => (
     <div className={styles['dropdown']}>
-        <div className={`${styles['dd-item']} ${styles['dd-selected']}`}>
+        <div className={`${styles['dd-item']} ${styles['dd-selected']} ${styles['dd-item-skeleton']}`}>
             <div className={styles['dd-selected-main']}>
                 <div className={styles['dd-icon-skeleton']} />
                 <div className={styles['dd-text-skeleton']} />
