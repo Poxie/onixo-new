cef3241ee0eeafdc836c93fce500f481bf3cb05e
= <poxen12@hotmail.com>
2023-05-18 22:02:53 +0200

    Displaying menu when navbar user is clicked

--- frontend/components/navbar/NavbarUser.tsx
+++ frontend/components/navbar/NavbarUser.tsx
@@ -3,6 +3,10 @@ import { useAuth } from "@/contexts/auth";
 import Button from "../button"
 import Image from "next/image";
 import { getUserAvatar } from "@/utils/getImages";
+import { useCallback, useRef } from 'react';
+import { useMenu } from '@/contexts/menu';
+import { MenuGroup } from '@/contexts/menu/types';
+import { useRouter } from 'next/router';
 
 const oauthUrl = `
 https://discord.com/oauth2/authorize
@@ -12,7 +16,28 @@ https://discord.com/oauth2/authorize
 &redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_REDIRECT_URI as string)}
 `.trim();
 export const NavbarUser = () => {
+    const router = useRouter();
     const { loading, user } = useAuth();
+    const { setMenu } = useMenu();
+
+    const ref = useRef<HTMLButtonElement>(null);
+
+    const logout = useCallback(() => {
+        localStorage.removeItem('access_token')
+        localStorage.removeItem('refresh_token')
+        window.location.href = '/';
+    }, []);
+    const showMenu = useCallback(() => {
+        const groups: MenuGroup[] = [
+            [
+                { text: 'Dashboard', onClick: () => router.push('/dashboard') },
+            ],
+            [
+                { text: 'Log out', onClick: logout, type: 'danger' }
+            ]
+        ]
+        setMenu(groups, ref);
+    }, []);
 
     if(loading) return null;
 
@@ -22,7 +47,11 @@ export const NavbarUser = () => {
                 Login with Discord
             </Button>
         ) : (
-            <button className={styles['user']}>
+            <button 
+                className={styles['user']}
+                onClick={showMenu}
+                ref={ref}
+            >
                 <Image 
                     src={getUserAvatar(user.id, user.avatar)}
                     width={24}
