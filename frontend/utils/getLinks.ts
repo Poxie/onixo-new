a5eb25ded7f7a5502c974f80d4c158d873ab5438
= <poxen12@hotmail.com>
2023-06-18 17:12:40 +0200

    Making sure links change based on client id env variable

--- frontend/utils/getLinks.ts
+++ frontend/utils/getLinks.ts
@@ -1,3 +1,3 @@
 export const getInviteLink = (guildId?: string) => (
-    `https://discord.com/oauth2/authorize?client_id=814312727115071499&scope=bot&permissions=8` + (guildId ? `&guild_id=${guildId}` : '')
+    `https://discord.com/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&scope=bot&permissions=8` + (guildId ? `&guild_id=${guildId}` : '')
 )
