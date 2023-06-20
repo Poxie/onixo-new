ad56e01e6980a5a56469c03b6db1c7da2aa7594b
= <poxen12@hotmail.com>
2023-06-21 00:02:55 +0200

    Made module sections a context - easier to interact with it

--- frontend/components/dashboard/module-section/index.tsx
+++ frontend/components/dashboard/module-section/index.tsx
@@ -1,6 +1,15 @@
-import { ArrowIcon } from '@/assets/icons/ArrowIcon';
+import React, { RefObject, useRef } from 'react';
 import styles from './ModuleSection.module.scss';
+import { ArrowIcon } from '@/assets/icons/ArrowIcon';
 import { useState } from 'react';
+
+export const ModuleContext = React.createContext({} as {
+    open: boolean;
+    setOpen: (state: boolean) => void;
+    ref: RefObject<HTMLDivElement>;
+})
+
+export const useModuleSection = () => React.useContext(ModuleContext);
 
 export const ModuleSection: React.FC<{
     header: string;
@@ -9,6 +18,13 @@ export const ModuleSection: React.FC<{
     className?: string;
 }> = ({ header, description, className, children }) => {
     const [open, setOpen] = useState(true);
+    const ref = useRef<HTMLDivElement>(null);
+
+    const value = {
+        setOpen,
+        open,
+        ref
+    }
 
     className = [
         styles['container'],
@@ -16,26 +32,28 @@ export const ModuleSection: React.FC<{
         className
     ].join(' ');
     return(
-        <div className={className}>
-            <button 
-                className={styles['header']}
-                onClick={() => setOpen(!open)}
-            >
-                <div className={styles['header-text']}>
-                    <h2>
-                        {header}
-                    </h2>
-                    <p>
-                        {description}
-                    </p>
-                </div>
-                <ArrowIcon />
-            </button>
-            <div className={styles['content-wrapper']}>
-                <div className={styles['content']}>
-                    {children}
+        <ModuleContext.Provider value={value}>
+            <div className={className} ref={ref}>
+                <button 
+                    className={styles['header']}
+                    onClick={() => setOpen(!open)}
+                >
+                    <div className={styles['header-text']}>
+                        <h2>
+                            {header}
+                        </h2>
+                        <p>
+                            {description}
+                        </p>
+                    </div>
+                    <ArrowIcon />
+                </button>
+                <div className={styles['content-wrapper']}>
+                    <div className={styles['content']}>
+                        {children}
+                    </div>
                 </div>
             </div>
-        </div>
+        </ModuleContext.Provider>
     )
 }
