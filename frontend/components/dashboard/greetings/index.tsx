56a3fce254df3fc56bbd7a3696da8b8825d0c059
= <poxen12@hotmail.com>
2023-06-21 23:00:58 +0200

    Fetching current welcome settings, added ability to update settings

--- frontend/components/dashboard/greetings/index.tsx
+++ frontend/components/dashboard/greetings/index.tsx
@@ -7,26 +7,72 @@ import { ModuleSection } from "../module-section"
 import { ItemList } from '../item-list';
 import { ModuleSubheader } from '../module-subheader';
 import { Input } from '@/components/input';
-import { useState } from 'react';
-import { useAppSelector } from '@/redux/store';
-import { selectChannelById, selectGuildById } from '@/redux/dashboard/selectors';
+import { useEffect, useRef, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/redux/store';
+import { selectChannelById, selectGuildById, selectWelcomeSettings } from '@/redux/dashboard/selectors';
 import { useGuildId } from '@/hooks/useGuildId';
-import Image from 'next/image';
-import { getCurrentTime } from '@/utils';
-import { replaceVariables } from '@/utils/variables';
 import { useAuth } from '@/contexts/auth';
 import { MessagePreview } from '../message-preview';
+import { ReduxWelcomeSettings } from '@/types';
+import { setWelcomeSettings, updateWelcomeSetting } from '@/redux/dashboard/actions';
 
 export const Greetings: NextPageWithLayout = () => {
     const guildId = useGuildId();
-    const { user } = useAuth();
+    const dispatch = useAppDispatch();
+    const { user, get, patch, token } = useAuth();
 
-    const [channelId, setChannelId] = useState<null | string>(null);
-    const [message, setMessage] = useState('');
-    const [directMessage, setDirectMessage] = useState('');
-
-    const channel = useAppSelector(state => selectChannelById(state, guildId, channelId as string));
+    const welcome = useAppSelector(state => selectWelcomeSettings(state, guildId))
+    const channel = useAppSelector(state => selectChannelById(state, guildId, welcome?.settings.channel as string));
     const guild = useAppSelector(state => selectGuildById(state, guildId));
+
+    const tempSettings = useRef(welcome?.settings);
+
+    useEffect(() => {
+        if(!token || !guildId) return;
+
+        get<ReduxWelcomeSettings['settings']>(`/guilds/${guildId}/welcome`, 'backend')
+            .then(settings => {
+                dispatch(setWelcomeSettings(guildId, settings));
+                tempSettings.current = settings;
+            })
+    }, [token, get, guildId]);
+
+    const sendUpdateRequest = (channelId?: string) => {
+        if(!welcome || !tempSettings.current) return;
+
+        // Determining what properties need to update
+        const propertiesToUpdate: {[key: string]: string} = {};
+        Object.entries(tempSettings.current).forEach(([key, value]) => {
+            const isSame = welcome.settings[key as keyof typeof welcome.settings] === value;
+            if(!isSame) {
+                propertiesToUpdate[key] = welcome.settings[key as keyof typeof welcome.settings];
+            }
+        });
+        
+        // If channelId is present
+        if(channelId !== undefined && (typeof channelId === 'string' || channelId === null)) propertiesToUpdate['channel'] = channelId as string;
+
+        // Checking if there are any properties to update
+        if(!Object.keys(propertiesToUpdate).length) return;
+
+        // Updating properties
+        patch(`/guilds/${guildId}/welcome`, propertiesToUpdate)
+            .then(() => {
+                tempSettings.current = welcome.settings;
+            })
+            .catch(() => {
+                if(!tempSettings.current) return;
+
+                // If request fails, restore to previous settings
+                dispatch(setWelcomeSettings(guildId, tempSettings.current))
+            })
+    }
+    const updateProperty = (property: keyof ReduxWelcomeSettings['settings'], value: any) => {
+        dispatch(updateWelcomeSetting(guildId, property, value));
+        if(property === 'channel') {
+            sendUpdateRequest(value);
+        }
+    }
 
     if(!user || !guild) return null;
     return(
@@ -47,7 +93,10 @@ export const Greetings: NextPageWithLayout = () => {
                     >
                         Welcome channel
                     </ModuleSubheader>
-                    <ItemList onChange={setChannelId} />
+                    <ItemList 
+                        onChange={channelId => updateProperty('channel', channelId)}
+                        defaultActive={welcome?.settings.channel}
+                    />
                     <ModuleSubheader
                         extraHeader={'Enter a message below to greet new members.'}
                         className={styles['header']}
@@ -56,12 +105,14 @@ export const Greetings: NextPageWithLayout = () => {
                     </ModuleSubheader>
                     <Input 
                         placeholder={'Welcome message'}
-                        onChange={setMessage}
+                        onChange={message => updateProperty('message', message)}
+                        defaultValue={welcome?.settings.message}
+                        onBlur={sendUpdateRequest}
                         textArea
                     />
                 </div>
                 <MessagePreview 
-                    message={message}
+                    message={welcome?.settings.message}
                     placeholder={'Enter a welcome message.'}
                     channelName={`# ${channel?.name || 'channel-not-selected'}`}
                 />
@@ -75,12 +126,14 @@ export const Greetings: NextPageWithLayout = () => {
                     </ModuleSubheader>
                     <Input 
                         placeholder={'Direct Message'}
-                        onChange={setDirectMessage}
+                        onChange={message => updateProperty('dm', message)}
+                        defaultValue={welcome?.settings.dm}
+                        onBlur={sendUpdateRequest}
                         textArea
                     />
                 </div>
                 <MessagePreview 
-                    message={directMessage}
+                    message={welcome?.settings.dm}
                     placeholder={'Enter a direct message.'}
                     channelName={`@ ${user.global_name}`}
                 />
