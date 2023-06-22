056b3db8873c0dda2522647100a3b6a52584d290
= <poxen12@hotmail.com>
2023-06-22 21:22:57 +0200

    Making sure not to use variables if undefined

--- frontend/utils/variables.ts
+++ frontend/utils/variables.ts
@@ -1,12 +1,16 @@
 import { Guild, User } from "@/types";
 import { getGuildIcon, getUserAvatar } from "./getImages";
 
-export const replaceVariables = (text: string, member: User, guild: Guild) => (
-    text
-        .replaceAll('[server]', guild.name)
-        .replaceAll('[sicon]', getGuildIcon(guild.id, guild.icon))
-        .replaceAll('[name]', member.global_name)
-        .replaceAll('[tag]', member.discriminator)
-        .replaceAll('[uicon]', getUserAvatar(member.id, member.avatar))
-        .replaceAll('|', '\n')
-)
+export const replaceVariables = (text: string, member: User, guild: Guild) => {
+    if(!member || !guild) return text;
+    
+    return(
+        text
+            .replaceAll('[server]', guild.name)
+            .replaceAll('[sicon]', getGuildIcon(guild.id, guild.icon))
+            .replaceAll('[name]', member.global_name)
+            .replaceAll('[tag]', member.discriminator)
+            .replaceAll('[uicon]', getUserAvatar(member.id, member.avatar))
+            .replaceAll('|', '\n')
+    )
+}
