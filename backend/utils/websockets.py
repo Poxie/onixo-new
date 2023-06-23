9e867b803be6554d40146ae7919d659d7d35ed8f
= <poxen12@hotmail.com>
2023-06-23 03:30:52 +0200

    Preventing error thrown on websocket close

--- backend/utils/websockets.py
+++ backend/utils/websockets.py
@@ -3,16 +3,19 @@ from websockets.sync.client import connect
 from flask import abort
 
 def send_message(type: str, payload=None):
-    with connect(f'ws://{os.getenv("WEBSOCKET_ORIGIN")}:{os.getenv("WEBSOCKET_PORT")}') as websocket:
-        message = {
-            'type': type,
-            'payload': payload
-        }
+    try:
+        with connect(f'ws://{os.getenv("WEBSOCKET_ORIGIN")}:{os.getenv("WEBSOCKET_PORT")}') as websocket:
+            message = {
+                'type': type,
+                'payload': payload
+            }
 
-        websocket.send(json.dumps(message))
+            websocket.send(json.dumps(message))
 
-        data = json.loads(websocket.recv())
-        if 'error' in data:
-            abort(data['error'], data['message'])
+            data = json.loads(websocket.recv())
+            if 'error' in data:
+                abort(data['error'], data['message'])
 
-        return data['response']
+            return data['response']
+    except:
+        pass
