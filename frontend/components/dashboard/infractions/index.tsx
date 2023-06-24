8dd3aa96e02a78f6abd9f4b1a58bcb44d1cc1d14
= <poxen12@hotmail.com>
2023-06-25 01:19:09 +0200

    Fetching and storing infractions, added dropdown to filter by action

--- frontend/components/dashboard/infractions/index.tsx
+++ frontend/components/dashboard/infractions/index.tsx
@@ -9,31 +9,52 @@ import { useAuth } from "@/contexts/auth";
 import { Infraction as InfractionType } from "@/types";
 import { Infraction } from './Infraction';
 import { Input } from '@/components/input';
+import { useAppDispatch, useAppSelector } from '@/redux/store';
+import { selectInfractions } from '@/redux/dashboard/selectors';
+import { setInfractions } from '@/redux/dashboard/actions';
+import { Dropdown } from '@/components/dropdown';
 
+const ACTION_FILTERS = [
+    { text: 'Ban', id: 'ban' },
+    { text: 'Kick', id: 'kick' },
+    { text: 'Mute', id: 'mute' },
+    { text: 'Warn', id: 'warn' },
+]
+
+const PLACEHOLDER_COUNT = 8;
 export const Infractions: NextPageWithLayout = () => {
+    const dispatch = useAppDispatch();
     const guildId = useGuildId();
     const { get, token } = useAuth();
 
-    const [infractions, setInfractions] = useState<InfractionType[] | undefined>(undefined);
     const [search, setSearch] = useState('');
+    const [action, setAction] = useState<string | null>(null);
+
+    const infractions = useAppSelector(state => selectInfractions(state, guildId));
     
     useEffect(() => {
         if(!token || !guildId) return;
 
         get<InfractionType[]>(`/guilds/${guildId}/infractions`, 'backend')
-            .then(setInfractions);
+            .then(infractions => {
+                dispatch(setInfractions(guildId, infractions));
+            });
     }, [token, get, guildId]);
     
     const filteredInfractions = useMemo(() => (
         infractions?.filter(infraction => {
-            if(!search) return infraction;
             return(
-                infraction.reason?.toLowerCase().includes(search.toLowerCase()) ||
-                infraction.target.global_name.toLowerCase().includes(search.toLowerCase()) ||
-                infraction.issuer.global_name.toLowerCase().includes(search.toLowerCase())
-            ) 
+                (search !== '' ? (
+                    infraction.reason?.toLowerCase().includes(search.toLowerCase()) ||
+                    infraction.target.global_name.toLowerCase().includes(search.toLowerCase()) ||
+                    infraction.issuer.global_name.toLowerCase().includes(search.toLowerCase())
+                ) : true) &&
+                (action ? (
+                    infraction.action === action
+                ) : true
+            ))
         })
-    ), [search])
+    ), [search, action, infractions?.length])
     return(
         <>
         <ModuleHeader 
@@ -44,12 +65,20 @@ export const Infractions: NextPageWithLayout = () => {
             <span className={styles['label']}>
                 Filters
             </span>
+            <div className={styles['flex']}>
+                <Dropdown 
+                    placeholder={'Filter by action'}
+                    items={ACTION_FILTERS}
+                    onChange={setAction}
+                    active={action}
+                />
+            </div>
             <Input 
                 placeholder={'Search'}
                 onChange={setSearch}
             />
         </div>
-        {filteredInfractions?.length !== 0 && (
+        {filteredInfractions && filteredInfractions?.length !== 0 && (
             <span className={styles['label']}>
                 Showing {filteredInfractions?.length} infractions
             </span>
@@ -68,6 +97,18 @@ export const Infractions: NextPageWithLayout = () => {
                     />
                 ))}
             </ul>
+        )}
+        
+        {/* Loading states */}
+        {!filteredInfractions && (
+            <>
+            <div className={styles['text-skeleton']} />
+            <div className={styles['list']}>
+                {Array.from(Array(PLACEHOLDER_COUNT)).map((_, key) => (
+                    <div className={`${styles['item']} ${styles['item-skeleton']}`} />
+                ))}
+            </div>
+            </>
         )}
         </>
     )  
