3fa65bc320d2b34b03a6e8d1f4f0967c6059efe1
= <poxen12@hotmail.com>
2023-06-27 20:29:00 +0200

    Added logging module to overview list

--- frontend/components/dashboard/overview/OverviewModules.tsx
+++ frontend/components/dashboard/overview/OverviewModules.tsx
@@ -1,11 +1,13 @@
 import styles from './Overview.module.scss'; 
 import { HammerIcon } from "@/assets/icons/HammerIcon"
 import { HandIcon } from "@/assets/icons/HandIcon"
+import { LoggingIcon } from '@/assets/icons/LoggingIcon';
 import { useGuildId } from "@/hooks/useGuildId"
 import Link from "next/link"
 
 const modules = [
     { text: 'Moderation', icon: <HammerIcon />, path: 'moderation/automod' },
+    { text: 'Logging', icon: <LoggingIcon />, path: 'logging' },
     { text: 'Welcomes & Goodbyes', icon: <HandIcon />, path: 'greetings' },
 ]
 
