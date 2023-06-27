6970bee89c38da917bcef72db7d8e9157e677b97
= <poxen12@hotmail.com>
2023-06-27 15:34:52 +0200

    Added new logging properties

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -3,7 +3,7 @@ ALLOWED_GOODBYE_PROPERTIES=['message', 'channel']
 ALLOWED_WELCOME_PROPERTIES=['message', 'dm', 'channel']
 AUTO_ROLE_PROPERTIES=['bots', 'users']
 ALLOWED_ANTILINK_SITES=['discord', 'youtube', 'twitter', 'twitch', 'facebook', 'instagram']
-ALLOWED_LOGGING_ACTIONS=[f'{action}_{"logs" if action == "all" else "log"}_channel' for action in ['all', 'ban', 'kick', 'mute', 'warn']]
+ALLOWED_LOGGING_ACTIONS=[f'{action}_{"logs" if action == "all" else "log"}_channel' for action in ['all', 'ban', 'kick', 'mute', 'warn', 'message', 'channel', 'user', 'server', 'voice']]
 ALLOWED_MOD_SETTINGS_PROPERTIES=['ephemeral', 'confirmation', 'sendDMs', 'incName', 'incReason']
 SEND_EMBED='SEND_EMBED'
 GET_TOP_ROLE='GET_TOP_ROLE'
