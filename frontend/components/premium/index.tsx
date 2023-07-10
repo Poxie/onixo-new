de5e3557ca0b972076c3a646e31c5ab420a31f1e
= <poxen12@hotmail.com>
2023-07-10 22:43:53 +0200

    Removed previous premium context

--- frontend/components/premium/index.tsx
+++ frontend/components/premium/index.tsx
@@ -1,29 +1,8 @@
 import styles from './Premium.module.scss';
 import Head from 'next/head';
 import { PremiumPlans } from './PremiumPlans';
-import React, { useCallback, useEffect, useState } from 'react';
-
-const PremiumContext = React.createContext({} as {
-    openCheckout: () => void;
-});
-
-export const usePremium = () => React.useContext(PremiumContext);
 
 export const Premium = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-
-        script.onload = () => {
-            (window as any).Chargebee.init({
-                site: 'onixo-test'
-            });
-            (window as any).Chargebee.registerAgain();
-        };
-
-        script.setAttribute('src', 'https://js.chargebee.com/v2/chargebee.js');
-        document.body.append(script)
-    }, []);
-
     return(
         <>
         <Head>
@@ -31,7 +10,7 @@ export const Premium = () => {
             <meta name="og:title" content="Onixo - Premium" />
             <meta name="description" content="We are currently working on delivering Onixo Premium. It will introduce many new features as well as improvements on current features." />
             <meta name="og:description" content="We are currently working on delivering Onixo Premium. It will introduce many new features as well as improvements on current features." />
-            <script src="https://js.chargebee.com/v2/chargebee.js" data-cb-site={process.env.NEXT_PUBLIC_CHARGEBEE_SITE}></script>
+            <script src="https://js.chargebee.com/v2/chargebee.js" data-cb-site={process.env.NEXT_PUBLIC_CHARGEBEE_SITE} defer></script>
         </Head>
 
         <div className={styles['container']}>
