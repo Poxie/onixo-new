4ec3bd8dee72c2eb36c4cd73c4c7b014030214de
= <poxen12@hotmail.com>
2023-07-12 11:46:32 +0200

    Added route to get hosted page for subscription checkout

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from blueprints.auth import auth
 from blueprints.guilds import guilds
 from blueprints.webhooks import webhooks
+from blueprints.subscriptions import subscriptions
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -16,6 +17,7 @@ CORS(app, resources={r'/*': {'origins': os.getenv('FRONTEND_ORIGIN')}})
 app.register_blueprint(auth)
 app.register_blueprint(guilds)
 app.register_blueprint(webhooks)
+app.register_blueprint(subscriptions)
 
 if __name__ == '__init__':
     app.run('127.0.0.1')
---
+++ backend/blueprints/subscriptions.py
@@ -0,0 +1,43 @@
+import chargebee, os
+from flask import Blueprint, jsonify, request, abort
+from dotenv import load_dotenv
+load_dotenv()
+
+chargebee.configure(os.getenv('CHARGEBEE_API_KEY'), os.getenv('CHARGEBEE_SITE'))
+
+subscriptions = Blueprint('subscriptions', __name__)
+
+@subscriptions.post('/guilds/<int:guild_id>/subscriptions/url')
+def get_subscription_url(guild_id: int):
+    plan_id = request.form.get('plan_id')
+    if not plan_id:
+        abort(400, 'plan_id is required')
+
+    entries = chargebee.ItemPrice.list()
+    prices = [entry.item_price for entry in entries]
+
+    plan = None
+    for price in prices:
+        if price.id == plan_id:
+            plan = price
+
+    if not plan:
+        abort(404, 'plan not found')
+
+    result = chargebee.HostedPage.checkout_new_for_items(dict(
+        subscription_items=[
+            dict(
+                item_price_id=plan.id,
+                unit_price=plan.price
+            )
+        ],
+        redirect_url=f'{os.getenv("FRONTEND_ORIGIN")}/dashboard/{guild_id}/premium' 
+    ))
+    page = result.hosted_page
+
+    return jsonify(dict(
+        id=page.id,
+        state=page.state,
+        created_at=page.created_at,
+        url=page.url,
+    ))
